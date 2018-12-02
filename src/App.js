e1c166852b0f94df07cecfa8002479fb4a7d2f65
Dejan <dekimioc@yahoo.com>
2018-12-02 23:57:43 +0100

    Added 4 components with styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import classes from "./App.css";
 import Header from './src/components/Header/Header';
+import About from './src/components/about/about';
+import Team from '../src/components/team/team';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <h1 className={classes.AboutText}>About us</h1>
+        <About />
+        <Team />
       </div>
     );
   }
---
+++ src/src/components/team/team.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as FontAwesome from 'react-icons/fa/index';
+
+const team = () => (
+    <div>
+        <h1>Our Team</h1>
+        <FontAwesome.FaArrowLeft />
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <FontAwesome.FaArrowRight />
+    </div>
+);
+
+export default team;
