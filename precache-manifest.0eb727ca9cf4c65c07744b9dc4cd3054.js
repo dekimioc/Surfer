9052d91d950ac9be475a7857d777ffd2dcabd356
Dejan Miocinovic <dekimioc@yahoo.com>
2020-02-27 23:32:03 +0100

    Updates

---
+++ precache-manifest.0eb727ca9cf4c65c07744b9dc4cd3054.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "b404834497e4c302f96e",
+    "url": "/Surfer/static/css/main.c36ce3f3.chunk.css"
+  },
+  {
+    "revision": "b404834497e4c302f96e",
+    "url": "/Surfer/static/js/main.b4048344.chunk.js"
+  },
+  {
+    "revision": "5a8ff66395409bc18c68",
+    "url": "/Surfer/static/js/1.5a8ff663.chunk.js"
+  },
+  {
+    "revision": "88e0776017ff4ab3ba7e",
+    "url": "/Surfer/static/js/runtime~main.88e07760.js"
+  },
+  {
+    "revision": "99c462df8c339d1f20e68b1083c347e3",
+    "url": "/Surfer/static/media/surfers-co_03.99c462df.png"
+  },
+  {
+    "revision": "775b39b1341b9eeb55f4a60868a64060",
+    "url": "/Surfer/static/media/about_back.775b39b1.png"
+  },
+  {
+    "revision": "dbf0ae2e63cc865fbb2d944d6eab8346",
+    "url": "/Surfer/static/media/Untitled-1.dbf0ae2e.png"
+  },
+  {
+    "revision": "21c32cddfed2e9f50ee38c5bb03fc11c",
+    "url": "/Surfer/static/media/header_back.21c32cdd.png"
+  },
+  {
+    "revision": "75e93fc056bc54982112664d742f6200",
+    "url": "/Surfer/static/media/surfer_preview.75e93fc0.png"
+  },
+  {
+    "revision": "f72b1d5ebd7339e09b81a14e1fb763ff",
+    "url": "/Surfer/static/media/surfer_2_preview.f72b1d5e.png"
+  },
+  {
+    "revision": "e307df4396b88112cda5ad3ab2b382d3",
+    "url": "/Surfer/static/media/surfer_3_preview.e307df43.png"
+  },
+  {
+    "revision": "48ff989fddffbcf699f5bd4b899473a9",
+    "url": "/Surfer/static/media/surfer_4_preview.48ff989f.png"
+  },
+  {
+    "revision": "d8cc154e629ad1273076f8ef05f9766e",
+    "url": "/Surfer/static/media/surfers-co_24.d8cc154e.png"
+  },
+  {
+    "revision": "dd0e7b5cddb6f8a300fe4dabfc88cff8",
+    "url": "/Surfer/static/media/surfers-co_29.dd0e7b5c.png"
+  },
+  {
+    "revision": "0338d6bbfe977f4d9c615361800af839",
+    "url": "/Surfer/static/media/surfers-co_26.0338d6bb.png"
+  },
+  {
+    "revision": "735d8560d6355d5d68667cd64e90e4c0",
+    "url": "/Surfer/static/media/surfers-co_32.735d8560.png"
+  },
+  {
+    "revision": "0009f2c8e321604cd6ee94cb5c797e27",
+    "url": "/Surfer/static/media/surfers-co_39.0009f2c8.png"
+  },
+  {
+    "revision": "37d94221afa534f8d64d7af327633dd3",
+    "url": "/Surfer/static/media/surfers-co_41.37d94221.png"
+  },
+  {
+    "revision": "604304a1c2eb2a06790ed7d79b8e7bc4",
+    "url": "/Surfer/static/media/surfers-co_43.604304a1.png"
+  },
+  {
+    "revision": "7cd0aaf45323596581e1cba00c4920ff",
+    "url": "/Surfer/static/media/surfers-co_49.7cd0aaf4.png"
+  },
+  {
+    "revision": "433606db1ef9fe4ec752cdc906a7047c",
+    "url": "/Surfer/static/media/surfers-co_50.433606db.png"
+  },
+  {
+    "revision": "cf4435958fa0d6b8c1b596613155187f",
+    "url": "/Surfer/static/media/surfers-co_52.cf443595.png"
+  },
+  {
+    "revision": "8a263176db8adb7fa3627190b120cabf",
+    "url": "/Surfer/static/media/surfers-co_53.8a263176.png"
+  },
+  {
+    "revision": "d7f6810b8a84e95fb5d8d3932a36a3dd",
+    "url": "/Surfer/index.html"
+  }
+];
